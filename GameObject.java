7cc46308b56a71637d41063eb159ac6c60a290f4
BeneficialPickle <40075115+BeneficialPickle@users.noreply.github.com>
2018-06-09 02:29:47 -0400

    Update GameObject.java
    Got rid of unused methods and instances.

--- GameObject.java
+++ GameObject.java
@@ -6,14 +6,14 @@ import java.awt.geom.Rectangle2D;
 import javax.swing.ImageIcon;
 
 public abstract class GameObject {
-    
+
     protected int x;
     protected int y;
     protected double xSpeed;
     protected double ySpeed;
     protected Image img;
     protected Rectangle2D hitbox;
-    
+
     public GameObject(int x, int y, double xSpeed, double ySpeed, Image img) {
         this.x = x;
         this.y = y;
@@ -22,56 +22,28 @@ public abstract class GameObject {
         this.img = img;
         hitbox = new Rectangle2D.Double(x, y, new ImageIcon(img).getIconWidth(), new ImageIcon(img).getIconHeight());
     }
-    
+
     public int getX() {
         return x;
     }
-    
+
     public int getY() {
         return y;
     }
-    
-    public double getXSpeed() {
-        return xSpeed;
-    }
-    
-    public double getYSpeed() {
-        return ySpeed;
-    }
-    
-    public Image getImage() {
-        return img;
-    }
-    
+
     public Rectangle2D getHitbox() {
         return hitbox;
     }
-    
+
     public void setX(int x) {
         this.x = x;
     }
-    
-    public void setY(int y) {
-        this.y = y;
-    }
-    
-    public void setXSpeed(double xSpeed) {
-        this.xSpeed = xSpeed;
-    }
-    
-    public void setYSpeed(double ySpeed) {
-        this.ySpeed = ySpeed;
-    }
-    
+
     public void setImage(Image img) {
         this.img = img;
     }
-    
-    public void setHitbox(Rectangle2D hitbox) {
-        this.hitbox = hitbox;
-    }
-    
+
     public abstract void update(ControlPanel background);
-    
+
     public abstract void paintComponent(Graphics g);
 }
